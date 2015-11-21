a6fcfa58620fb025f066eac399ce18e3b02e8adf
Attila Tovt <uran198@gmail.com>
2015-11-21 17:47:53 +0200

    add User.avatar resizing on save

--- med/users/models.py
+++ med/users/models.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
+from PIL import Image
 
-from django.contrib.auth.models import AbstractUser
 from django.core.urlresolvers import reverse
+from django.core.files.images import File
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
+
+from django.contrib.auth.models import AbstractUser
 
 from .utils import upload_path
 from .validators import validate_file_size
@@ -26,3 +29,13 @@ class User(AbstractUser):
 
     def get_absolute_url(self):
         return reverse('users:detail', kwargs={'username': self.username})
+
+    def save(self, *args, **kwargs):
+        if self.avatar:
+            image = Image.open(self.avatar.file)
+            wsize = 100
+            hsize = int(wsize / image.size[0] * image.size[1])
+            image = image.resize((wsize, hsize), Image.ANTIALIAS)
+            image.save(self.avatar.path)
+            self.avatar = File(open(self.avatar.path, 'rb'))
+        return super(User, self).save(*args, **kwargs)
--- med/users/tests/test_validators.py
+++ med/users/tests/test_validators.py
@@ -8,12 +8,12 @@ from ..validators import validate_file_size
 class TestFileValidation(TestCase):
 
     def test_valid_file_size(self):
-        f = UploadedFile(size=10000)
+        f = UploadedFile(size=1000000)
         try:
             validate_file_size(f)
         except ValidationError:
             self.fail("validate_file_size raised ValidationError unexpectedly!")
 
     def test_invalid_file_size(self):
-        f = UploadedFile(size=10001)
+        f = UploadedFile(size=1000001)
         self.assertRaises(ValidationError, validate_file_size, f)
