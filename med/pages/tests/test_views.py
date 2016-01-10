0123561e3f94a8774da3ade482e9b0327a7650b1
Attila Tovt <uran198@gmail.com>
2016-01-10 18:11:10 +0200

    Added redirect from home page to questions:list when logged in

--- med/pages/tests/test_views.py
+++ med/pages/tests/test_views.py
@@ -1,3 +1,27 @@
-# from django.test import TestCase
+from django.test import RequestFactory
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import AnonymousUser
 
-# Create your tests here.
+from test_plus.test import TestCase
+
+from .. import views
+
+
+class HomeViewTest(TestCase):
+
+    def setUp(self):
+        self.user = self.make_user()
+        self.factory = RequestFactory()
+        self.view = views.HomeView.as_view()
+        self.request = self.factory.get('fake/')
+
+    def test_user_authenticated(self):
+        self.request.user = self.user
+        response = self.view(self.request)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.get('Location'), reverse('questions:list'))
+
+    def test_user_not_authenticated(self):
+        self.request.user = AnonymousUser()
+        response = self.view(self.request)
+        self.assertEqual(response.status_code, 200)
--- med/pages/views.py
+++ med/pages/views.py
@@ -1,8 +1,14 @@
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
     template_name = 'pages/home.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            return redirect('questions:list')
+        return super(TemplateView, self).dispatch(request, *args, **kwargs)
 
 
 class AboutView(TemplateView):
