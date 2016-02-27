a1b7f6456d2031a4b6e0eb2f1c52318d9b249815
Attila Tovt <uran198@gmail.com>
2016-02-27 12:44:42 +0200

    Removed archive from navigation

--- med/users/forms.py
+++ med/users/forms.py
@@ -3,7 +3,7 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class SignupForm(forms.Form):
-    is_doctor = forms.BooleanField(label=_("I'm a medical specialist"))
+    is_doctor = forms.BooleanField(required=False, label=_("I'm a medical specialist"))
 
     def signup(self, request, user):
         user.is_doctor = self.cleaned_data['is_doctor']
