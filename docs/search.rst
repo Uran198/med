ae4651defb59df51dd44043bb45580be3ecec70c
Attila Tovt <uran198@gmail.com>
2016-04-02 17:05:03 +0300

    Removed files not needed for designing a prototype

--- docs/search.rst
+++
@@ -1,6 +0,0 @@
-http://django-haystack.readthedocs.org/en/v2.4.1/tutorial.html
-
-Cron Job::
-   #!/bin/bash
-
-   su - jacob -c "/django/jacob/med/manage.py update_index"
--- med/users/validators.py
+++
@@ -1,9 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-# TODO: to use custom size not built in should use
-# class validators: https://docs.djangoproject.com/en/1.8/ref/validators/
-def validate_file_size(f):
-    size = 1000000
-    if f.size > size:
-        raise ValidationError("File too large")
